1ab4d61987f1682a3867da6a7823d79da12767a5
Enver Fransazov <161112453+FransazovEnver@users.noreply.github.com>
2026-01-26 12:15:47 +0200

    test commit for DemoSelenium

---
+++ DemoSeleniumWebDriver/DemoSWD/UnitTest1.cs
@@ -0,0 +1,23 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DemoSWD
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestWikipediaUrl()
+        {
+            //Create chrome driver and open new chrome browser
+            IWebDriver driver = new ChromeDriver();
+
+            //Navigate to Url
+            driver.Navigate().GoToUrl("https://wikipedia.org");
+        }
+    }
+}
