aa469c369eb6962f7b4cdc62be63649a64dc0a6a
Enver Fransazov <161112453+FransazovEnver@users.noreply.github.com>
2026-01-27 11:59:01 +0200

    end of the Selenium Demo 27.01.26

--- DemoSeleniumWebDriver/DemoSWD/UnitTest1.cs
+++ DemoSeleniumWebDriver/DemoSWD/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace DemoSWD
             //Navigate to Url
             driver.Navigate().GoToUrl("https://wikipedia.org");
 
+            //Print page title
+            Console.WriteLine("Main page title: " + driver.Title);
+
+            //Logate the search input
+            var searchBox = driver.FindElement(By.Name("search"));
+
+            //Write a text in seachBox and push the enter
+            searchBox.SendKeys("Quality Assurance" + Keys.Enter);
+
+            //Print the page title
+            Console.WriteLine("Title after search: " + driver.Title);
+            
             //close the browser
             driver.Quit();
         }
