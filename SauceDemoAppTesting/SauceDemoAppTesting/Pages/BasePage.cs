1671deff475ae89fb079a986840e84b81dd38e5d
Enver Fransazov <enverfransazov@gmail.com>
2026-07-01 09:39:03 +0300

    commit

--- SauceDemoAppTesting/SauceDemoAppTesting/Pages/BasePage.cs
+++ SauceDemoAppTesting/SauceDemoAppTesting/Pages/BasePage.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SauceDemoAppTesting.Pages
 {
-    internal class BasePage
+    public class BasePage
     {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        protected IWebElement FindElement(By locator)
+        {
+            return wait.Until(d => d.FindElement(locator));
+        }
     }
 }
