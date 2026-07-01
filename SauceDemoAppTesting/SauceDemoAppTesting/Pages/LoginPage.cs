acba99fb0ad9585389802f7f61abc7931c47d972
Enver Fransazov <enverfransazov@gmail.com>
2026-07-01 10:07:44 +0300

    commit

--- SauceDemoAppTesting/SauceDemoAppTesting/Pages/LoginPage.cs
+++ SauceDemoAppTesting/SauceDemoAppTesting/Pages/LoginPage.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SauceDemoAppTesting.Pages
 {
-    internal class LoginPage
+    public class LoginPage : BasePage
     {
+        private readonly By usernameInput = By.XPath("//input[@name='user-name']");
+        private readonly By userpasswordInput = By.XPath("//input[@name='password']");
+        private readonly By loginButton = By.XPath("//input[@name='login-button']");
+        private readonly By errorMassege = By.XPath("//h3[@data-test='error']");
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+            
+        }
     }
 }
